bcc5b1707d94cddeb763e76bc4423a461e44f651
Andrew Porter <haporter@me.com>
2022-03-04 16:57:55 -0700

    PatientResourceType model implementation

--- Patient Feedback demo/Patient Feedback demo/Model/PatientResourceType.swift
+++ Patient Feedback demo/Patient Feedback demo/Model/PatientResourceType.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct PatientResourceType: Codable {
+    struct Address: Codable {
+        let use: String
+        let line: [String]
+    }
+    
+    struct Contact: Codable {
+        let system: String
+        let value: String
+        let use: String
+    }
+    
+    let id: UUID
+    let name: [Name]
+    let active: Bool
+    let contact: [Contact]
+    let gender: String
+    let birthDate: String
+    let address: [Address]
+}

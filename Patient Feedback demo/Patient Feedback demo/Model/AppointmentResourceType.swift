2a86bb70ccc3ba87912dbf71075afe8ead9db43e
Andrew Porter <haporter@me.com>
2022-03-04 16:56:53 -0700

    AppointmentResourceType model implementation

--- Patient Feedback demo/Patient Feedback demo/Model/AppointmentResourceType.swift
+++ Patient Feedback demo/Patient Feedback demo/Model/AppointmentResourceType.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct AppointmentResourceType: Codable {
+    struct AppointmentPeriod: Codable {
+        let start: Date
+        let end: Date
+    }
+    struct AppointmentType: Codable {
+        let text: String
+    }
+    
+    let id: UUID
+    let status: String
+    let type: [AppointmentType]
+    let subject: ResourceReference
+    let actor: ResourceReference
+    let period: AppointmentPeriod
+}

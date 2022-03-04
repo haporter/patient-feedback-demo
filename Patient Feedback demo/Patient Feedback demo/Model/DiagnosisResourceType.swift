33b3876149e0093a01cc52e471f67b7507770479
Andrew Porter <haporter@me.com>
2022-03-04 16:56:35 -0700

    DiagnosisResourceType model implementation

--- Patient Feedback demo/Patient Feedback demo/Model/DiagnosisResourceType.swift
+++ Patient Feedback demo/Patient Feedback demo/Model/DiagnosisResourceType.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct DiagnosisResourceType: Codable {
+    struct MedicalCodes: Codable {
+        struct Coding: Codable {
+            let system: URL
+            let code: String
+            let name: String
+        }
+        
+        let coding: [Coding]
+    }
+    struct MetaData: Codable {
+        let lastUpdated: Date
+    }
+    
+    let id: UUID
+    let meta: MetaData
+    let status: String
+    let code: MedicalCodes
+    let appointment: ResourceReference
+}

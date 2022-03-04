28d64215374678a3a41f75f13584630d3bbc0c61
Andrew Porter <haporter@me.com>
2022-03-04 10:02:23 -0700

    Xcode project added

---
+++ Patient Feedback demo/Patient Feedback demo/Patient_Feedback_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Patient_Feedback_demoApp.swift
+//  Patient Feedback demo
+//
+//  Created by Andrew Porter on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Patient_Feedback_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

e5df0b4033ec0e78202b0e529cb5961d61683e31
Andrew Porter <haporter@me.com>
2022-03-05 13:18:29 -0700

    WIP FeedbackFlowViewModel implementation

--- Patient Feedback demo/Patient Feedback demo/FeedbackFlowViewModel.swift
+++ Patient Feedback demo/Patient Feedback demo/FeedbackFlowViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+class FeedbackFlowViewModel: ObservableObject {
+    
+    enum State {
+        case idle(Error?)
+        case inProgress(Color)
+        case finished
+    }
+    
+    @Published var showBackButton = true
+    @Published var state: State = .idle(nil)
+}

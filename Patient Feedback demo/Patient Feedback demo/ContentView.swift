743fd0777b11bb158f5f131a7cce43a0b5ba5cc2
Andrew Porter <haporter@me.com>
2022-03-07 10:27:50 -0700

    Bundle decoder helper

--- Patient Feedback demo/Patient Feedback demo/ContentView.swift
+++ Patient Feedback demo/Patient Feedback demo/ContentView.swift
@@ -25,7 +25,7 @@ struct ContentView: View {
     }
     
     func testDecoding() {
-        let medicalRecord = Bundle.main.decode(TEMedicalRecord.self, from: "patient-feedback-raw-data.json", dateDecodingStrategy: .iso8601)
+        let medicalRecord = Bundle.main.decode(TEMedicalRecord.self, from: "patient-feedback-raw-data.json")
         print(medicalRecord)
     }
 }
@@ -33,36 +33,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-
-extension Bundle {
-    func decode<T: Decodable>(_ type: T.Type, from file: String, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate, keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
-        guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Failed to locate \(file) in bundle.")
-        }
-
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to load \(file) from bundle.")
-        }
-
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = dateDecodingStrategy
-        decoder.keyDecodingStrategy = keyDecodingStrategy
-
-        do {
-            return try decoder.decode(T.self, from: data)
-        } catch DecodingError.keyNotFound(let key, let context) {
-            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' not found – \(context.debugDescription)")
-        } catch DecodingError.typeMismatch(_, let context) {
-            fatalError("Failed to decode \(file) from bundle due to type mismatch – \(context.debugDescription)")
-        } catch DecodingError.valueNotFound(let type, let context) {
-            fatalError("Failed to decode \(file) from bundle due to missing \(type) value – \(context.debugDescription)")
-        } catch DecodingError.dataCorrupted(_) {
-            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON")
-        } catch {
-            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
-        }
     }
 }

97405e54da443564c3dc275d96ca3fb0371733bc
Andrew Porter <haporter@me.com>
2022-03-07 10:29:22 -0700

    Coordinator implementation

--- Patient Feedback demo/Patient Feedback demo/Feedback flow/FeedbackFlowCoordinator.swift
+++ Patient Feedback demo/Patient Feedback demo/Feedback flow/FeedbackFlowCoordinator.swift
@@ -9,4 +9,21 @@ import SwiftUI
 
 struct FeedbackFlowCoordinator {
     
+    let viewModel: FeedbackFlowViewModel
+    
+    func navigateFeedback(for index: Int?) {
+        guard let index = index else { return viewModel.state = .finished }
+        
+        if index <= 0 {
+            viewModel.state = .inProgress(.recommend)
+        } else if index > FeedbackItem.allCases.count - 1 {
+            viewModel.state = .finished
+        } else {
+            viewModel.state = .inProgress(FeedbackItem.allCases[index])
+        }
+    }
+    
+    func loadInitalState() {
+        viewModel.state = .inProgress(.recommend)
+    }
 }
